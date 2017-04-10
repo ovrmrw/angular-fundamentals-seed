2d4260061cb24856eaf39c157aba66c44acd65b5
ovrmrw <t.ngc.0403@gmail.com>
2017-04-10 22:17:13 +0900

    check point

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [
-    AppComponent
-  ],
   declarations: [
-    AppComponent
-  ]
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
